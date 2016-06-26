883ecadfcf3172e34338af6e9d8f4cdac224e8c0
JacopoLibe <jacopo.libe@gmail.com>
2016-06-26 10:48:25 +0200

    Added NuGet package auto build

--- SteamVR_HUDCenter/Properties/AssemblyInfo.cs
+++ SteamVR_HUDCenter/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
 [assembly: AssemblyTitle("SteamVR_HUDCenter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OpenVR and SteamVR API to draw overlays from .NET applications")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Artum")]
 [assembly: AssemblyProduct("SteamVR_HUDCenter")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
